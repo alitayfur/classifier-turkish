59fe61e67154dcefaa353f5486f8876acc36f326
ali tayfur <alitayfur@msn.com>
2019-03-31 12:31:32 +0300

    firt load
    Signed-off-by: ali tayfur <alitayfur@msn.com>

---
+++ classifier.php
@@ -0,0 +1,93 @@
+﻿<?php
+
+	class classifier {
+		public $data = array();
+		
+		public function classify($string,$thing= 'turkish')
+				{
+			    $this->data['scala']  = [];
+				$this->data['string'] = $string;			
+				$this->sentence();
+			foreach ($this->data['sentence'] as $sentence):
+				$last['word']         = end($sentence);
+				$last['add5']         = mb_substr($last['word'],-5,4);
+				$last['add2']         = mb_substr($last['word'],-2,2);
+				$last['add1']         = mb_substr($last['word'],-1,1);
+ 
+				$this->data['scala'][]   = $this->opinion($last,$sentence,$thing);
+			
+		    endforeach;
+		
+		    return $this->data['score'] = round(((100/(count($this->data['scala'])*100))*array_sum($this->data['scala'])));
+		    
+
+				}
+
+
+
+		public function smile() {       
+			$smile= ceil($this->data['score']/20);
+	        return $smile<1?1:$smile;
+		}
+		
+		
+		public function sentence() {
+			$this->data['sentence'] =[];
+			$sentences = explode('.',$this->data['string']);
+			foreach ($sentences as $sentence):
+				$words = explode(',',$sentence);
+				foreach ($words as $word) {
+					$word = trim($word);
+					if ($word=='') continue;
+					$this->data['sentence'][] = explode(' ',$word);
+										  }			
+			endforeach;
+
+
+		}
+		
+
+
+
+	    private function opinion($last, $sentence, $thing) {
+	    	$return=100;
+	    	$letters['turkish'] = array(
+							'add1' => array('k','u','ı','ü'),
+							'add2' => array('uk','ok','ak','tu','tı','tü'),
+							'add5' => array('madı','medi')
+							);
+	    	$beds['turkish']  = array('medi','madı','mamı','mami','hiç ','hic ',"mez",'maz','kötü','siz','vasat','sız','yok','berbat','eski','kirli','mıyor','miyor','mama','yazık','tiksi','pisli','fırça','fırca','neden',' ne ','niye','mem','mam');
+	    	$goods['turkish'] = array('kemmel','mutlu','var','beğendim','begendim','ekkür','tebr');
+	    	foreach ($letters[$thing] as $key => $value) {
+	    				if (in_array($last[$key], $value)) $return-=40;
+	    		}
+
+	    	$text = $sentence;
+	    	unset($text[count($text)-1]);
+	    	$text = implode(' ',$text);
+
+	    	foreach ($beds[$thing] as $bed) {
+	    		$return-=(80*$this->str_pos($text,$bed));
+	    	}
+
+	    	foreach ($goods[$thing] as $good) {
+	    		$return+=(80*$this->str_pos($text,$good));
+	    	}
+
+	    	return $return;
+
+	    }
+
+		private function str_pos($haystack ,$needle ,$offset=0,$location=0) {
+				$location = strpos($haystack ,$needle ,$location);
+				if ($location !== false) {
+					$offset++;
+					$location++;
+					$this->str_pos($haystack ,$needle ,$offset,$location);
+				}
+				return $offset;
+
+			}
+
+		}
+
---
+++ test.php
@@ -0,0 +1,69 @@
+<?php
+include "classifier.php";
+$classifier = new classifier;
+
+$comments[]="Biz genel olarak memnun kaldık. Yemekleri yenilebilir kalitede menü zengin. Çok kalabalık bi otel olmasına rağmen sıra derdi yok. Yemeğe en son gitseniz bile menüden hiç bir yemek bitmiyor. 15 aylık oğlumuz havuzda çok su yuttu hastalanır diye korktuk ama hiç hastalanmadı. Benim kulağımda kötü bir havuza girersem iltihaplanma olur. Goldcity de olmadı.
+Gündüz animasyonları biraz zayıf kalıyor. Akşamları idare eder.
+Denize uzak ama servisi var. Biz deniz için gitmediğimizden dert etmiyoruz. Kumsalı çok iyi değil alanya denizi malum dalgalı ve çabuk derinleşiyor.
+Ama tesiste 10 numara aqua park var.U hariç sıra derdi yok. U kaydırakta da bi numara yok aslında diğerleri çok çok daha zevkli zaten. Havuzların sayısı çok ve de büyük. Şezlongta yer kapma derdi yok. Önde yer bulamazsan yanda bulursun. Havuz başı snack barda yapılan köfteler pizzalar ve gözlemeler gayet güzeldi. İçecek sıkıntısı yok ve sırası makul seviyede 1-2 dakkada içeceğinizi alabilirsiniz. Ama tek sıra dondurmada oluyordu.
+Suit odada amerikan mutfak var. Çamaşır bulaşık makinesi mikro dalga fırın katlle ütü ne ararsanız var. Bebekli aileler için ideal . Mama pişirme süt ısıtma vs eşim çok rahat etti. Bebek için bebek yatağı getirdiler. Evimizdekinin aynısı oğlum çok rahat etti.
+Alakart restoran ise çok zayıf. Göstermelik diyebilirim. Alakart yerine açık büfeyi yercih ederim şahsen.
+Kısmetse yine gelmeyi düşünürüz.";
+
+$comments[]="Görüntüye kanıp rezervasyon yaptırdık heleki eğer bir Türk iseniz çok fazla bi beklentiniz olmasın hersey yabancılara etkinlik vasat işe yarar bi aqua var oda günlük 3 4 saat ancak açık kalıyor disko gece 12 den sonra açılıyor ki herşey dahil konseptine ait de olsanız diskoda para ödeyin diye...Kendi çapınızda eğlenip yinede bişeylerin tadını çıkaralım dedik bu sefer yemekleri izin vermedi iki aile gittiğimiz otelde bir günümüzü de zehirlenme sonucu topluca hastanede geçirdik..Kısacası 6 günlük rezervasyonda 1. gün giriş 14 olmasına rağmen akşam 5 de girdik 2-3-4 günler sıradan ve sıkıcı 5. gün hastalıkla geçti 6.gün sabah kalkıp hastalık üstüne toparlamaya çalışırken girişimizi 5 de yapan resepsiyon görevlileri saat 12 olmadan telefonla odayı boşaltmamız için tacize başlamaları 5 yıldız çook çook fazla bu otele";
+$comments[]= "Herşey için Aqua Plaza otele çok teşekkür ederim 5 gece kaldım herşey umdugum gibiydi butun personel can gonulden tebrik ederim işlerini güler yüzüyle misafirlerin memnuniyeti doğrultusunda severek yapiyorlar özellikle animasyonlar muhteşemdi animasyinda çalışan elemanlar (batuhan ve Monika ve de huseyine ayrica tesekkurlerimi sunarim Otel içinde bize yardımcı olan belboy hasan garson barış ve arkadaşlarıni (122) nolu odamizi temizleyen h/k sultan hanima herkeze teşekkürlerimizi sunarım birdaki seneye görüşmek üzere hoşçakalin";
+
+$comments[] = "Bir sosyologla calisarak degiskenleri kategorizelendirerek baslamak. Sahsi görüşüm realite % 5 in uzerine cikamaz";
+
+$comments[] = "Deniz'e uzak, tepe de olduğu için soğuk, tadilat nedeniyle kapalı havuzun ısıtması yok, dolayısıyla havuza doğru düzgün giremedik... tadilat olduğu için çıkış yapmak istedik ilk gün ücreti alırız dediler.. jest için suit oda verdiler ama normal odadan pek fazlası yoktu. kliması çalışmıyordu yapılması iki gün aldı... son günde oteli kapatıyorlarmıydı artık neydi kahvaltıda herşey bayattı... ya yanlış Zaman'da gittik ya da otel gerçekten kötü... sadece lobi barda baş barmen iyiydi...";
+
+
+$comments[] = "wikingeni tercih ettik balayı için otelde ne bir balayı odası verildi otele gittik yerimiz yok denildi 2 ay önceden rezerve ve ücretini ödemiştim. balayı konsepti görmedik hiçbir şekilde çalışanlar yabancı turistlerle ilgileniyor yerli turistlerle alakaları yok. havuzlar pislik içinde eşim insan pisliği görmüş içinde tiksindim iyice bu otelden. ve ben 1 haftalık tatil almıştım 4 günlük bol odamız var dediler.";
+$comments[] = 'Mimarisini en cok begendigim otel denize bakan odalarda balkona ciktiginizda kendinizi bir crouse gemisinde yolculuk yapar gibi hissediyorsunuz. Odalar gayet kaliteli malzemeler kullanilarak dizayn edilmis hersey icin tesekkurler vikingen ';
+
+$comments[] = "Slm şuan otelden yazıyorum ve 2ci gecemiz öncelikle karşılama gayet güzel saat 11 olmasına rağmen hemen odamıza yerleştik benim ve arkadaşlarımızın çoçuklarımız var cocuk icin aktivite ve özel ayarlanmis yemek bölümü cok hoş genel yemekler biraz eksik yani ben şahsen cok cesit sevmem ama damak tadina düskün olanlar sıkıntı yaşar bize yeterliydi havuzlara gelince gayet temiz ve fazlasiyla yeterli deniz...";
+$comments[] = "Genel olarak oteli beğendim yemekleri ve içiçekleri tabi ki süper olmasada tatmin ediciydi herkesin yerden yere vurduğu kadarda değil.Denizede hep iskeleden girdigimiz icin gayet keyıflıydı , temizlikte iyidi ama gel gelelim ki persenolin küfürlü konuşup yabancı turistlerin içine düşüp biz birşey söylediğimizde yüzümüze bile bakmaması çok irite edici bi durum acilen buna cözüm bulmaları lazım ha bide bi kac tane... ";
+$comments[] = "Otel gerçekten çok kötü ben iki genç arkadaş la gitmeme rağmen hiç beğenmedik ve üstelik de bir gün erken dönüş yaptık... hani iyi bir şeyi olsa yazıcam gerçekten özellik le yerli turist için çok ilgisizler... hic temizlik yok ve kirli bir otel...havuz berbat ve cok kirli. .. klima hiç düzgün çalışmıyor. .. hep sıcaktan catliyorsunuz uyurken gece kendiliginden kapanıyor klima......";
+
+$comments[] = "otel, fiyatına göre çok çok iyi. yeme içme konusu ise zirvede. yemekler harika..bol bol tatil fotoğrafı çekilebilecek hoş bir ortamı ve dekoru var.tek kötü yanı havuz öyle bi otele göre çok küçük. deniz çok derin olduğu için zaten hiç kullanılamıyor.biz hergün incekum a gitmek zorunda kalmıstık. denize derinlikten ötürü giremeyince havuzu tercih etmek gerekiyor ama dediğim gibi havuz da çok... ";
+
+
+$comments[] = "Ağustos ayının ilk haftasında eşim ve 6 yaşındaki kızım ile birlikte tasarımı ve lokasyonu mükemmel olan bu otelde konakladık.
+Öncelikle saat 13:40 civarı otele geldik giriş işlemlerimizi yaptıktan sonra resepsiyondaki bayan bizi gayet kibar bir şekilde yemeğe yönlendirdi ve yemek yedikten sonra geldiğimizde odamızın hazır olduğunu belirtti ve valizlerimizle beraber odamıza yerleştik.
+Öncelikle odadan bahsedecek olursak gayet ferah ve iç açıcı dizayn edilmişti,yataklar gerçekten çok rahattı ve manzarası oldukça güzeldi (1350 noluoda)
+Havuzlar gerçekten güzel ancak bence biraz dezenfekte sorunu bence vardı çünkü tatil dönüşü kolumda birkaç yara çıktı buna ragmen havuzlar büyüklüğü ve fazlalığı sayesinde geçer not aldı.Aqua Parkı hiç kullanmadım bu konu hakkında yorum yapamayacağım. Plaj ise bence güzeldi ancak deniz için o kadar temiz diyemem plaja yakın olan bölgede ufak ufak pislikler vardı ama su güzeldi açıldıkça.Denizden çıkınca yıkanma yerleri vardı ancak çok amatörceydi.
+Yemek ve ikram hizmetleri için ızgara dışında olumsuz cümle kuramayacağım yemekler oldukça çeşitli ve lezzeti harikuladeydi. Ancak ızgara işi o kadar olumlu not alamadı zaten 1 haftada 2-3 kere aldım onlarıda zaten yemedim. Ağırlıklı olarak tavuk kanat vardiki oda tabiri caizse saman gibiydi. Kahvaltı çok güzeldi onlarca çeşit kahvaltılık arasında aç kalmak mümkün değildi. Bunların dışında ana binanın olduğu bölümdeki dondurma ve pastane hizmeti oldukça güzel ve yeterliydi.Sanırım yoğun bir otel olduğu için dondurma sayısını kişi başı olarak 2 adet olarak sınırlandırmaları bence mantıklıydı. Ancak gözleme sırasında o kadar uzun süre bekliyorsunki insan sinirleniyor burdaki bayanda rica minnet yapar gibi tavırlarıda hoş değildi. Açıkçası bakışlarından rahatsız olmamak elde değildi, bu konuda sınıfta kaldı bence otel. Agua parkın ordaki snack bar adlandırılan bölge on numaraydı aç kalmak mümkün değildi.Hamburger Pizza Nugget Kumpir Makarna vs. gerçekten zengin bir ortam yaratıyordu.
+Eğlence işine gelirsek gündüz havuz oyunları normal çok fazla bir ekstra durum yoktu ancakkkk akşam saat 21.00 itibari ile önce çocuk eğlenceleri daha sonra 21:45 de tiyatro yarışma gösteri tarzındaki aktiviteler tam not aldı 23:00 itibariylede canlı müzik plaj partısı vs. seylerde geceyi uzun tutmak isteyenler için oldukça cezbedici yanlardı.Sadece gündüz çalınan müziklerin yelpazesini biraz değil baya genişletmeleri gerekir. 1 hafta boyunca aynı şarkılar çaldı durdu. Ama genel olarak animasyon ekibi görevini başarıyla gerçeklestirdi benim gözümde.
+Bir eksi not ise aile oteli konsepttindeki grup halindeki bekalr erkeklerdi.
+Genel anlamda olumlu yanlarının olumsuz yanlarına göre oldukça ağır bastığı bir oteldi. ";
+$comments[] = " (-) havz başı şezlong kavgası
+ucuz adi dandik alkollü alkolsüz içecekler
+ilk okul mezunu bile olmayan çalışanlar ve kadro
+20 adet arabalık oto park
+(+) süper ötesi yemek
+çok iyi oda
+güzel pastane
+otel görünüm çok iyi havuz çok güzel
+Oda İpucu: denize bakan ön kısım çok iyi ";
+
+$comments[] = "Torunum ve gelinimle gittiğim oteldi. Çalışanlar ilgili-alakalı ve sanki evimizde gibiydik. Sabahları jimnastik hareketleri ile güne başlıyorduk. Kahvaltı, torunuma havuz keyfi öğlen yemeği, deniz sefası. Gelinim ve torunum aqua parktan kayıyorlardı. Bahçesinde hamaklar prensesimizin uykusu gelince hamakta uyuyordu, oda temizliği mükemmel. Yataklarımızın üstü her gün değişik figürlerle süsleniyordu. Güzel ortam, ne içtiğinizi garsonlar biliyor anında yemek masasında...Beğenmiş olduğum bir ortamdı... ";
+$comments[] = "daha bikaç saat önce otelden çıkışımı yaptırdım ama içim gitti. çalışanlarının güler yüzünden mi bahsetsem yoksa otelin temizliğinden mi bilemedim, ama eşinizle dostunuzla sevgilinizle gidip mutlu bir şekilde ayrılacağınız bi otel. her şeyin bedava olması insanı ayrı bir cezbediyor tabii ki ";
+$comments[] = "Nerden başlasam. Otel girişinde Yelda isimli Reception müdürü girer girmez fırçayla karşılıyor sizi. Ne deseniz size bağırarak cevap veriyorlar. Size söylenen barbekü ve Alakart hakkınızı ise asla kullandırtmayorlar. Özellikle Türk turiste resmen mürteci muamelesi yapılıyor. Gelelim odalara. son derece eski ve bakımsız- banyo kapısı kapanmıyor. Yataklar o kadar eski ki. sabah sırt ve bel ağrısı ile uyanıyorsun. Yemekler berbat. Sadece... ";
+
+$comments[] ="yemek kalitesi iyi değil
+sauna hamam bölümü kötü, özellikle temizliği
+tercih ettiğimiz içecek sadece mohito,onu da içemedik,
+bahane : nane bitti :))";
+$comments[] ="ezervasyonumuzu balayı odası olarak ayırmıştık,ama otelin bu konuda iyi bir organisazyonu olmadı,hayal kırıklığı yaşadık,personelin suratsızlığı ve ilgisizliği bizi adeta üzdü,alakart restaurant berbat yemekler özellikle etler ve pilav bozuk ve kokuyordu.
+
+눇otelin konumu güzel..odalar konforlu ve rahat.";
+
+$comments[] = '2 yıl önce 1 hafta kaldığımız ve çok memnun olduğumuz için herkese tavsiye ettiğim bir otel olmuştu. Kardeşim , annem ve yeğenim benim tavsiyem üzerine dün (18.08.2015) giriş yaptıkları otelde çok saygısız davranışlara maruz kalmışlar. Bu kadar güzel bir tesiste çalışmak üzere neden eleman seçilirken dikkat edilmez anlamıyorum. Özellikle annem için sarfedilen cümleleri duyunca yazıklar olsun dedim. Elemanlarınızı seçerken turizm... ';
+
+
+
+foreach ($comments as $comment) {
+    $rate = $classifier->classify($comment);
+echo '(<b>'.$rate.'</b>/100)<img src="smile/'.$classifier->smile().'.png"> '.$comment.'<br>
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------<br>';
+}
